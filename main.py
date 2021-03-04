73c6fc7c9f1bc33f5459ef5369dad0437347cb9c
Antonio <antoniodainok@gmail.com>
2021-03-04 11:01:58 +0100

    postgre setup

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ load_dotenv('.env')
 
 # App configuration
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 ckeditor = CKEditor(app)
